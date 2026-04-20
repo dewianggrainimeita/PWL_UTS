de42a97abe849566eed0b6c021f19e573af285a0
Dewi Anggraini Meita Sari <ssitiaisya2@gmail.com>
2026-04-20 22:38:14 +0700

    create migration table pos

---
+++ database/migrations/2026_04_20_152401_create_stoks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_stok', function (Blueprint $table) {
+        $table->id('stok_id');
+        $table->foreignId('supplier_id');
+        $table->foreignId('barang_id');
+        $table->foreignId('user_id');
+        $table->dateTime('stok_tanggal');
+        $table->integer('stok_jumlah');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stoks');
+    }
+};
---
+++ database/migrations/2026_04_20_152440_create_penjualan_details_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_penjualan_detail', function (Blueprint $table) {
+        $table->id('detail_id');
+        $table->foreignId('penjualan_id');
+        $table->foreignId('barang_id');
+        $table->integer('harga');
+        $table->integer('jumlah');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('penjualan_details');
+    }
+};
